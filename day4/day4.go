27973e8add8dbe5b4c0354e1de21343e1f19cf9f
jecepeda <jcepedavillamayor@gmail.com>
2020-12-04 18:59:17 +0100

    finish day 4

---
+++ day4/day4.go
@@ -0,0 +1,171 @@
+package day4
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Passport map[string]string
+
+// NewPassport creates a new passport from the line
+func NewPassport(line string) Passport {
+	passport := make(Passport)
+	fragments := strings.Split(line, " ")
+	for _, f := range fragments {
+		parts := strings.Split(f, ":")
+		passport[parts[0]] = parts[1]
+	}
+	return passport
+}
+
+// Contains checks the validity of the password
+func (p Passport) Contains(fields []string) bool {
+	for _, f := range fields {
+		if _, ok := p[f]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+// Validate validates the password
+func (p Passport) Validate(validations ...ValidationFunction) bool {
+	for _, v := range validations {
+		if !v(p) {
+			return false
+		}
+	}
+	return true
+}
+
+// ValidationFunction is a function used to validate passports
+type ValidationFunction func(p Passport) bool
+
+func validYear(field string, min, max int) ValidationFunction {
+	return func(p Passport) bool {
+		strField, ok := p[field]
+		if !ok {
+			return false
+		}
+		intField, err := strconv.Atoi(strField)
+		if err != nil {
+			return false
+		}
+		return intField >= min && intField <= max
+	}
+}
+func validHeight(p Passport) bool {
+	strHeight, ok := p["hgt"]
+	if !ok {
+		return false
+	}
+	if strings.Contains(strHeight, "cm") {
+		strHeight = strings.ReplaceAll(strHeight, "cm", "")
+		height, err := strconv.Atoi(strHeight)
+		if err != nil {
+			return false
+		}
+		return height >= 150 && height <= 193
+	} else if strings.Contains(strHeight, "in") {
+		strHeight = strings.ReplaceAll(strHeight, "cm", "")
+		height, err := strconv.Atoi(strHeight)
+		if err != nil {
+			return false
+		}
+		return height >= 59 && height <= 76
+	}
+	return false
+}
+
+func validHairColor(p Passport) bool {
+	hair, ok := p["hcl"]
+	if !ok {
+		return false
+	}
+	match, err := regexp.Match(`^\#([0-9a-f]{6})$`, []byte(hair))
+	if err != nil {
+		return false
+	}
+	return match
+}
+
+func validColors(colors ...string) ValidationFunction {
+	return func(p Passport) bool {
+		color, ok := p["ecl"]
+		if !ok {
+			return false
+		}
+		for _, c := range colors {
+			if c == color {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+func validPassportID(p Passport) bool {
+	passportID, ok := p["pid"]
+	if !ok {
+		return false
+	}
+	match, err := regexp.Match(`^([0-9]{9})$`, []byte(passportID))
+	if err != nil {
+		return false
+	}
+	return match
+}
+
+// FirstPart checks the passports contained on the lines
+// and returns the ones that are valid
+func FirstPart(lines []string) int {
+	passports := cleanPassports(lines)
+	var valid int
+
+	for _, p := range passports {
+		if p.Contains([]string{"ecl", "eyr", "hcl", "byr", "iyr", "pid", "hgt"}) {
+			valid++
+		}
+	}
+
+	return valid
+}
+
+func SecondPart(lines []string) int {
+	passports := cleanPassports(lines)
+	var valid int
+
+	for _, p := range passports {
+		if p.Validate(
+			validYear("byr", 1920, 2002),
+			validYear("iyr", 2010, 2020),
+			validYear("eyr", 2020, 2030),
+			validHeight,
+			validHairColor,
+			validColors("amb", "blu", "brn", "gry", "grn", "hzl", "oth"),
+			validPassportID,
+		) {
+			valid++
+		}
+	}
+
+	return valid
+}
+
+func cleanPassports(lines []string) []Passport {
+	var result []Passport
+	for i := 0; i < len(lines); i++ {
+		var line string
+		for i < len(lines) && lines[i] != "" {
+			if line == "" {
+				line = lines[i]
+			} else {
+				line += " " + lines[i]
+			}
+			i++
+		}
+		result = append(result, NewPassport(line))
+	}
+	return result
+}
---
+++ day4/day4_test.go
@@ -0,0 +1,132 @@
+package day4
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/jecepeda/advent-code-2020/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFirstPart(t *testing.T) {
+	tests := []struct {
+		name string
+		path string
+		want int
+	}{
+		{
+			name: "test case",
+			path: "./test.txt",
+			want: 2,
+		},
+		{
+			name: "real part 1",
+			path: "./input.txt",
+			want: 164,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			lines, err := util.ReadFile(tt.path)
+			require.NoError(t, err)
+			if got := FirstPart(lines); got != tt.want {
+				t.Errorf("FirstPart() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSecondPart(t *testing.T) {
+	tests := []struct {
+		name string
+		path string
+		want int
+	}{
+		{
+			name: "test case",
+			path: "./test.txt",
+			want: 2,
+		},
+		{
+			name: "real part 1",
+			path: "./input.txt",
+			want: 101,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			lines, err := util.ReadFile(tt.path)
+			require.NoError(t, err)
+			if got := SecondPart(lines); got != tt.want {
+				t.Errorf("SecondPart() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestInvalidPassports(t *testing.T) {
+	tests := []struct {
+		name    string
+		rawLine string
+		want    int
+	}{
+		{
+			name: "test case",
+			rawLine: `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007`,
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			lines := strings.Split(tt.rawLine, "\n")
+			if got := SecondPart(lines); got != tt.want {
+				t.Errorf("InvalidPassports() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidPassports(t *testing.T) {
+	tests := []struct {
+		name    string
+		rawLine string
+		want    int
+	}{
+		{
+			name: "test case",
+			rawLine: `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`,
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			lines := strings.Split(tt.rawLine, "\n")
+			if got := SecondPart(lines); got != tt.want {
+				t.Errorf("InvalidPassports() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
