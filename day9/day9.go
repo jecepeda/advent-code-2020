c04165901b277cfd55c293882802c28b7a0095e0
jecepeda <jcepedavillamayor@gmail.com>
2020-12-09 10:25:32 +0100

    finish day9

---
+++ day9/day9.go
@@ -0,0 +1,48 @@
+package day9
+
+import "sort"
+
+func SecondPart(numbers []int, preamble int) int {
+	num := FindNotMatchingNumber(numbers, preamble)
+
+	sumNumbers := FindSumNumbers(numbers, num)
+	sort.Slice(sumNumbers, func(i, j int) bool {
+		return sumNumbers[i] > sumNumbers[j]
+	})
+	return sumNumbers[0] + sumNumbers[len(sumNumbers)-1]
+}
+
+func FindSumNumbers(numbers []int, num int) []int {
+	for i := range numbers {
+		var n = numbers[i]
+		for j := i + 1; j < len(numbers) && n < num; j++ {
+			n += numbers[j]
+			if n == num {
+				return numbers[i : j+1]
+			}
+		}
+	}
+	return nil
+}
+
+// FindNotMatchingNumber finds the number which is not possible
+// to sum with the n preamble numbers
+func FindNotMatchingNumber(numbers []int, preamble int) int {
+	for i := preamble; i < len(numbers); i++ {
+		if !matchNumber(numbers[i], numbers[i-preamble:i]) {
+			return numbers[i]
+		}
+	}
+	return -1
+}
+
+func matchNumber(n int, preamble []int) bool {
+	for i := range preamble {
+		for j := range preamble {
+			if preamble[i]+preamble[j] == n {
+				return true
+			}
+		}
+	}
+	return false
+}
---
+++ day9/day9_test.go
@@ -0,0 +1,68 @@
+package day9
+
+import (
+	"testing"
+
+	"github.com/jecepeda/advent-code-2020/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFindNotMatchingNumber(t *testing.T) {
+	tests := []struct {
+		name     string
+		path     string
+		preamble int
+		want     int
+	}{
+		{
+			name:     "test",
+			path:     "./test.txt",
+			want:     127,
+			preamble: 5,
+		},
+		{
+			name:     "input",
+			path:     "./input.txt",
+			want:     31161678,
+			preamble: 25,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			numbers, err := util.FileToIntList(tt.path)
+			require.NoError(t, err)
+			got := FindNotMatchingNumber(numbers, tt.preamble)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestSecondPart(t *testing.T) {
+	tests := []struct {
+		name     string
+		path     string
+		preamble int
+		want     int
+	}{
+		{
+			name:     "test",
+			path:     "./test.txt",
+			want:     62,
+			preamble: 5,
+		},
+		{
+			name:     "input",
+			path:     "./input.txt",
+			want:     5453868,
+			preamble: 25,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			numbers, err := util.FileToIntList(tt.path)
+			require.NoError(t, err)
+			got := SecondPart(numbers, tt.preamble)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
